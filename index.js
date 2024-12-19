c5a19b50b034f0b82450b05b8e1fa1666d330a21
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-19 12:44:40 +0100

    midleware setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("express")();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const db = require("./config/db");
+const consign = require("consign");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+consign().then("./config/middlewares.js").into(app);
+
+app.db = db;
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
